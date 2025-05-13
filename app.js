70e8403329a498dffcb982440e8349d7ffbd3b2c
Jami Karimkulov <jami.karimkul@gmail.com>
2025-05-12 20:10:47 -0400

    Set up basic Express server with routes

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
